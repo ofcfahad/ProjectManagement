11d551778fe366b70bda71c108c69653830a9ede
Fahad <fahadbilalbhatti16@gmail.com>
2023-06-05 04:48:36 -0700

    modified:   server/middlewares/checkSession.ts

--- server/middlewares/checkSession.ts
+++ server/middlewares/checkSession.ts
@@ -3,7 +3,6 @@ import jwt from 'jsonwebtoken'
 
 const checkSession = (req: Request, res: Response, next: NextFunction) => {
     const sessionCookie = req.headers.authorization;
-    console.log(sessionCookie);
     const secret = process.env.SECRET_KEY
     if (sessionCookie && jwt.verify(sessionCookie, secret!)) {
         next();
